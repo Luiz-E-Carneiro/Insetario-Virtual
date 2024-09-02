647f0f089d795b5a77c5afd007d12cd8317ac032
Luiz-E-Carneiro <luizecarneiro2006@gmail.com>
2024-09-01 22:49:48 -0300

    Header and Footer style

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,73 +1,71 @@
 import { RiInstagramFill, FaGithub, FaSquareXTwitter } from "react-icons/fa";
-
 import logo_if from "./../../assets/brands/logo_if.png";
 import logo_insetario from "./../../assets/brands/logo_insetario.png";
 
 export default function Footer() {
     return (
-        <>
-            <footer class="w-full backdrop-blur-md">
-                <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
-                    <div class="md:flex md:justify-between">
-                        <div class="mb-6 md:mb-0">
-                            <div>
-                                <img src={logo_insetario} class="h-8 me-3" alt="Logo do Projeto" />
-                                <span class="self-center text-2xl font-serif font-semibold whitespace-nowrap ">Insetário Virtual</span>
-                                <img src={logo_if} class="h-8 me-3" alt="Logo do Projeto" />
-                            </div>
-                            <p className="text-justify whitespace-nowrap">
-                                Projeto de extensão do IFRS-Campus Bento Gonçalves de um insertário virtual
-                                com intuito de auxiliar alunos de cursostécnicos que envolvam agronomia e
-                                comunidade em geral na indentificação de insetos.
-                            </p>
+        <footer className="w-full bg-white py-8">
+            <div className="px-10 max-sm:p-4">
+                <div className="flex flex-col md:flex-row md:justify-between gap-x-20 gap-y-8">
+                    <div className="flex flex-col md:w-2/5">
+                        <div className="flex items-center gap-4 mb-4">
+                            <img src={logo_insetario} className="h-10 rounded-full" alt="Logo do Insetário Virtual" />
+                            <span className="text-lg font-serif font-semibold">Insetário Virtual</span>
+                            <img src={logo_if} className="h-8" alt="Logo do IFRS" />
                         </div>
-                        <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
-                            <div>
-                                <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase ">Navegação</h2>
-                                <ul class="text-gray-500 font-medium">
-                                    <li class="mb-4">
-                                        <a href="https://google.com" class="hover:underline">Sobre o Projeto</a>
-                                    </li>
-                                    <li>
-                                        <a href="https://google.com" class="hover:underline">Insetário</a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div>
-                                <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercas">Contate-nos</h2>
-                                <ul class="text-gray-500">
-                                    <li class="mb-4">
-                                        <a href="https://google.com" class="hover:underline ">email_exemplo@gmail.com</a>
-                                    </li>
-                                    <li>
-                                        <a href="https://google.com" class="hover:underline">(54) 99999-9999</a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div>
-                                <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercas">Siga-nos</h2>
-                                <ul class="text-gray-500">
-                                    <li class="mb-4">
-                                        <span><RiInstagramFill /> Instagram </span>
-                                    </li>
-                                    <li>
-                                        <span><FaSquareXTwitter /> X</span>
-                                    </li>
-                                    <li>
-                                        <span><FaGithub /> GitHub</span>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
+                        <p className="text-sm text-gray-600">
+                            Projeto de extensão do IFRS-Campus Bento Gonçalves de um insetário virtual com intuito de
+                            auxiliar alunos de cursos técnicos que envolvam agronomia e comunidade em geral na
+                            identificação de insetos.
+                        </p>
                     </div>
-                    <hr class="my-6 border-gray-200 sm:mx-auto lg:my-8" />
-                    <div class=" flex justify-between">
-                        <span class="text-sm text-gray-500 sm:text-center">
-                            © 2024 Insetário Virtual. Todos Direitos Reservados.
-                        </span>
+
+                    <div className="grid grid-cols-2 gap-4 sm:gap-6 md:w-2/3">
+                        <div>
+                            <h2 className="mb-4 text-sm font-semibold text-gray-900 uppercase">Navegação</h2>
+                            <ul className="text-gray-600 space-y-2">
+                                <li>
+                                    <a href="https://google.com" className="hover:underline">Sobre o Projeto</a>
+                                </li>
+                                <li>
+                                    <a href="https://google.com" className="hover:underline">Insetário</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h2 className="mb-4 text-sm font-semibold text-gray-900 uppercase">Contate-nos</h2>
+                            <ul className="text-gray-600 space-y-2">
+                                <li>
+                                    <a href="mailto:email_exemplo@gmail.com" className="hover:underline">email_exemplo@gmail.com</a>
+                                </li>
+                                <li>
+                                    <a href="tel:+54999999999" className="hover:underline">(54) 99999-9999</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h2 className="mb-4 text-sm font-semibold text-gray-900 uppercase">Siga-nos</h2>
+                            <ul className="text-gray-600 space-y-2">
+                                <li>
+                                    <span><RiInstagramFill /> Instagram</span>
+                                </li>
+                                <li>
+                                    <span><FaSquareXTwitter /> X</span>
+                                </li>
+                                <li>
+                                    <span><FaGithub /> GitHub</span>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-            </footer>
-        </>
+                <hr className="my-6 border-gray-200" />
+                <div className="flex flex-col sm:flex-row justify-between items-center text-center sm:text-left">
+                    <span className="text-sm text-gray-500">
+                        © 2024 Insetário Virtual. Todos Direitos Reservados.
+                    </span>
+                </div>
+            </div>
+        </footer>
     );
 }
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,20 +1,51 @@
+import React, {useState} from "react";
 import logo_if from "./../../assets/brands/logo_if.png";
 import logo_insetario from "./../../assets/brands/logo_insetario.png";
 
-export default function Header() {
-    return(
-        <>
-            <header className="w-full h-12 box-border shadow-xl py-2 px-4 font-serif font-semibold flex justify-between items-center">
-                <div className="flex items-center gap-2">
-                    <img src={logo_insetario} alt="Logo do Projeto" className="h-full" />
-                    <h1 className="text-xl">Insetario Virtual</h1>
+
+export default function Header () {
+    const Links = [
+        { name: "Sobre o Projeto", link: "/" },
+        { name: "Insetário", link: "/" }
+    ];
+    const [open, setOpen] = useState(false);
+
+    return (
+        <header className="fixed top-0 left-0 w-full h-14 shadow-md z-30 bg-white">
+            <div className="flex items-center justify-between h-full px-10 max-sm:p-4">
+
+                <div className="flex items-center gap-2 font-bold text-lg sm:text-xl md:text-2xl cursor-pointer">
+                    <img src={logo_insetario} alt="Logo do Insetário Virtual" className="h-11 sm:h-12 rounded-full" />
+                    <span>Insetário Virtual</span>
                 </div>
-                <div>
-                    <p>Sobre o Projeto</p>
-                    <p>Insetario</p>
+
+                <nav
+                    className={`absolute sm:static top-14 sm:top-0 right-0 w-fit sm:w-auto sm:flex items-center bg-white sm:bg-transparent transition-all duration-500 ease-in-out overflow-hidden ${open ? 'max-h-[300px] opacity-100' : 'max-h-0 opacity-0'} sm:max-h-none sm:opacity-100`}>
+                    <ul className="flex flex-col gap-4 p-4 sm:flex-row sm:items-center w-fit sm:w-auto">
+                        {Links.map((link, index) => (
+                            <li key={index} className="font-semibold">
+                                <a href={link.link} className="text-gray-800 hover:text-[#688A41] transition duration-500">
+                                    {link.name}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+
+
+                <div className="hidden sm:flex items-center gap-4">
+                    <img
+                        src={logo_if}
+                        alt="Logo do IFR"
+                        className={`h-11 transition duration-500 ${open ? 'hidden' : 'block'}`}
+                    />
                 </div>
-                <img src={logo_if} alt="Logo do IF" className="h-full" />
-            </header>
-        </>
+                <button
+                    onClick={() => setOpen(!open)}
+                    className={`sm:hidden flex items-center px-2 py-1 rounded-sm transition duration-100 hover:bg-slate-200 ${open ? 'z-50' : 'z-20'}`}>
+                    {open ? "Fechar" : "Abrir"}
+                </button>
+            </div>
+        </header>
     );
-}
+};
