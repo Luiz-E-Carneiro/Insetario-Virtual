12d5cbdbfe8a1c5a58d842df435c5a37d7e23104
Luiz-E-Carneiro <luizecarneiro2006@gmail.com>
2024-09-07 20:18:25 -0300

    Form Style: Responsiveness

--- frontend/src/pages/Insectarium/Form.jsx
+++ frontend/src/pages/Insectarium/Form.jsx
@@ -36,58 +36,63 @@ export default function Form({ sendFormData }) {
     return (
         <div className="form-container bg-black/[.25] w-full h-fit rounded px-4 py-3 mt-4 backdrop-blur-sm z-10 text-white">
             <h1 className="text-xl font-semibold">Filtragem</h1>
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 mt-4">
-                <div className="flex gap-2 items-center text-nowrap">
-                    <label htmlFor="commonName" className='font-semibold'>Nome Comum:</label>
-                    <input
-                        type="text"
-                        id="commonName"
-                        name="commonName"
-                        value={formData.commonName}
-                        onChange={handleChange}
-                        className="w-full bg-transparent border-b-2 border-white outline-none"
-                    />
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+                <div className='col-span-1 flex flex-col justify-between gap-4'>
+                    <div className="flex gap-2 items-center text-nowrap">
+                        <label htmlFor="commonName" className='font-semibold'>Nome Comum:</label>
+                        <input
+                            type="text"
+                            id="commonName"
+                            name="commonName"
+                            value={formData.commonName}
+                            onChange={handleChange}
+                            className="w-full bg-transparent border-b-2 border-white outline-none"
+                        />
+                    </div>
+                    <div className="flex gap-2 items-center text-nowrap">
+                        <label htmlFor="scientificName" className='font-semibold'>Nome Científico:</label>
+                        <input
+                            type="text"
+                            id="scientificName"
+                            name="scientificName"
+                            value={formData.scientificName}
+                            onChange={handleChange}
+                            className="w-full bg-transparent border-b-2 border-white outline-none"
+                        />
+                    </div>
+
                 </div>
-                <div className="flex gap-2 items-center text-nowrap">
-                    <label htmlFor="scientificName" className='font-semibold'>Nome Científico:</label>
-                    <input
-                        type="text"
-                        id="scientificName"
-                        name="scientificName"
-                        value={formData.scientificName}
-                        onChange={handleChange}
-                        className="w-full bg-transparent border-b-2 border-white outline-none"
-                    />
+                <div className='col-span-1 flex flex-col justify-between gap-4'>
+                    <div className="flex gap-2 items-center text-nowrap">
+                        <label htmlFor="ordem" className='font-semibold'>Ordem:</label>
+                        <select
+                            id="ordem"
+                            name="ordem"
+                            value={formData.ordem}
+                            onChange={handleChange}
+                            className="w-full bg-white/25 border-b-2 border-white outline-none"
+                        >
+                            <option value="" disabled>Selecionar</option>
+                            <option value="ordem1">Ordem 1</option>
+                            <option value="ordem2">Ordem 2</option>
+                        </select>
+                    </div>
+                    <div className="flex gap-2 items-center text-nowrap">
+                        <label htmlFor="family" className='font-semibold'>Família:</label>
+                        <select
+                            id="family"
+                            name="family"
+                            value={formData.family}
+                            onChange={handleChange}
+                            className="w-full bg-white/25 border-b-2 border-white outline-none"
+                        >
+                            <option value="" disabled>Selecionar</option>
+                            <option value="family1">Família 1</option>
+                            <option value="family2">Família 2</option>
+                        </select>
+                    </div>
                 </div>
-                <div className="flex gap-2 items-center text-nowrap">
-                    <label htmlFor="ordem" className='font-semibold'>Ordem:</label>
-                    <select
-                        id="ordem"
-                        name="ordem"
-                        value={formData.ordem}
-                        onChange={handleChange}
-                        className="w-full bg-white/25 border-b-2 border-white outline-none"
-                    >
-                        <option value="" disabled>Selecionar</option>
-                        <option value="ordem1">Ordem 1</option>
-                        <option value="ordem2">Ordem 2</option>
-                    </select>
-                </div>
-                <div className="flex gap-2 items-center text-nowrap">
-                    <label htmlFor="family" className='font-semibold'>Família:</label>
-                    <select
-                        id="family"
-                        name="family"
-                        value={formData.family}
-                        onChange={handleChange}
-                        className="w-full bg-white/25 border-b-2 border-white outline-none"
-                    >
-                        <option value="" disabled>Selecionar</option>
-                        <option value="family1">Família 1</option>
-                        <option value="family2">Família 2</option>
-                    </select>
-                </div>
-                <div className="h-5 sm:col-span-2 flex gap-6">
+                <div className="h-6 md:h-full sm:col-span-2 flex lg:col-span-1 lg:flex-col lg:justify-between gap-4">
                     <div className="flex items-center">
                         <label htmlFor="predador" className='font-semibold'>Predador:</label>
                         <input
@@ -100,7 +105,7 @@ export default function Form({ sendFormData }) {
                         />
                     </div>
                     {!formData.predador && (
-                        <div className="w-full sm:w-auto flex-grow flex gap-2 items-center text-nowrap">
+                        <div className="w-full sm:w-auto flex-grow flex flex-col sm:flex-row gap-2 items-start text-nowrap">
                             <label htmlFor="culture" className='font-semibold'>Cultura atacada:</label>
                             <select
                                 id="culture"
